ca1152da583f7d204e9b4acdaf159c8a9af7d140
HalissonJulio <92337947+HalissonJulio@users.noreply.github.com>
2021-10-20 13:49:55 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,49 +1,41 @@
 window.onload = function() {
 
-    const elementos = document.querySelectorAll("h2");
-    const lista = document.querySelector("ol")
+	const elementos = document.querySelectorAll("h2");
+	const lista = document.querySelector("ol")
 
-    elementos.forEach(elemento => {
-        
-        const listaLi = document.createElement("li");
-        const link = document.createElement("a");
+	elementos.forEach(elemento => {
+		
+		const listaLi = document.createElement("li");
+		const link = document.createElement("a");
 
-        link.textContent = elemento.textContent
-        listaLi.appendChild(link)
-        lista.append(listaLi)
+		link.textContent = elemento.textContent
+		listaLi.appendChild(link)
+		lista.append(listaLi)
 
-    })
+		const h3 = document.createElement("h3")
+		const retorno = document.createElement("a")
 
-    const paragrafos = document.querySelectorAll("p")
-    paragrafos.forEach(paragrafo => {
+		retorno.textContent = "Clique aqui para voltar à lista"
+		h3.appendChild(retorno)
 
-        const h3 = document.createElement("h3")
-        const retorno = document.createElement("a")
+		elemento.parentElement.insertBefore(h3, elemento.nextSibling)
+	})
 
-        retorno.textContent = "Clique aqui para voltar à lista"
-        h3.appendChild(retorno)
-
-        paragrafo.parentElement.insertBefore(h3, paragrafo.nextSibling)
-    })
-
-
-    const links = document.querySelectorAll("a")
-    links.forEach(link => {
-        link.onclick = function() {
-            if(link.innerHTML == "Clique aqui para voltar à lista") {
-                document.querySelector("h3").scrollIntoView()
-            } else {
-                let destino;
-                for (i = 0; i < elementos.length; i++) {
-                    if (elementos[i].textContent == link.textContent) {
-                        destino = elementos[i]
-                        break
-                    }
-                }
-                destino.scrollIntoView()
-            }
-        }
-    })
-
-    
+	const links = document.querySelectorAll("a")
+	links.forEach(link => {
+		link.onclick = function() {
+			if(link.innerHTML == "Clique aqui para voltar à lista") {
+				document.querySelector("h3").scrollIntoView()
+			} else {
+				let destino;
+				for (i = 0; i < elementos.length; i++) {
+					if (elementos[i].textContent == link.textContent) {
+						destino = elementos[i]
+						break
+					}
+				}
+				destino.scrollIntoView()
+			}
+		}
+	})
 }
